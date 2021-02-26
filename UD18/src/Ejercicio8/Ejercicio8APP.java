af5bbddd3bb43720edce5e972ce2f297b76aab73
MathiasDebrun <mathiasdebrun@hotmail.com>
2021-02-26 15:39:40 +0100

    Commit3.1

--- UD18/src/Ejercicio8/Ejercicio8APP.java
+++ UD18/src/Ejercicio8/Ejercicio8APP.java
@@ -1,10 +1,21 @@
 package Ejercicio8;
 
-public class Ejercicio8APP {
+import java.sql.Connection;
 
+import Ejercicio5.Conexion;
+
+public class Ejercicio8APP {
+	public static Connection conexion;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Conexion.openConnection();
+		CrearBD.createDB("BDUD18EJ8");
+//		CrearTablas.createTableDespacho("BDUD18EJ3","Despacho");
+//		CrearTablas.createTableDirectores("BDUD18EJ3","Directores");
+//		InsertarDatos.insertDespacho("BDUD18EJ3","Despacho", "Despacho1", 15);
+//		InsertarDatos.insertDirector("BDUD18EJ3", "Directores", "12345678A", "Juan Pérez", "12345678A", 1 );
+		Conexion.closeConnection();
+		
 	}
 
 }
